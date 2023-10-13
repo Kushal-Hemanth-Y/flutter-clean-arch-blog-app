54b38f9b76a766c5982dd9a82776c15cbfba2b6e
Kushal-Hemanth-Y <kushalhemanthy@gmail.com>
2023-10-13 18:34:13 +0530

    required dependencies added in pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class JournoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
